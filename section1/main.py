7edfbca218f9592efad1d83d2da85f6d6807416f
minsubkim2 <gpgp0330@naver.com>
2022-08-17 18:57:27 +0900

    aaa

--- section1/main.py
+++ section1/main.py
@@ -1,1 +1,4 @@
-print("Hello World")
+print("Hello World!")
+print("Day 1 -python print Function")
+print("The function is declared like this:")
+print("print('what tp print')")
---
+++ section1/main2.py
@@ -0,0 +1,4 @@
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + "Angela")
+print("Hello" + " " + "Angela")
+
