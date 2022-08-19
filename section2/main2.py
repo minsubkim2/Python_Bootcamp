2e66d8e451ae9f6fab095d5a3b289ba1fa1ae548
minsubkim2 <gpgp0330@naver.com>
2022-08-19 11:53:46 +0900

    int

---
+++ section2/main2.py
@@ -0,0 +1,8 @@
+two_digit_number = input("Type a two digit nuber: ")
+#print(type(two_digit_number))
+#print("Hello"[0])
+first_digit = two_digit_number[0]
+seccond_digit = two_digit_number[1]
+
+result = int(first_digit) + int(seccond_digit)
+print(result)
