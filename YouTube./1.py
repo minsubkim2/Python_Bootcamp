63ed6948bab69924a202cca0f7a1b20fe7ff2fbd
minsubkim2 <gpgp0330@naver.com>
2022-08-22 19:58:53 +0900

    YouTube

---
+++ YouTube./1.py
@@ -0,0 +1,19 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# print('풍선')
+# print("나비")
+# print("ㅋ"*9)
+
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
