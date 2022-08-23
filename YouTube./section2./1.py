1bde540e48314b66b54731ee1128cf08687dcdd9
minsubkim2 <gpgp0330@naver.com>
2022-08-23 18:50:35 +0900

    aa

---
+++ YouTube./section2./1.py
@@ -0,0 +1,32 @@
+#  연산 
+ 
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) #2*2*2
+print(5%3) # 나머지 구하기 2
+print(10%3) # 1
+print(5//3) # 5를 3으로 나누었을때 몫 1
+print(10//3) # 3
+
+print(10 > 3) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(4 == 2)  # False
+
+
+print(1 != 3) # 1은 3과 같지 않다  = not
+print(not(1 !=3))
+
+print((3 > 0) and (3 < 5)) #둘다 같아야 True가 나온다 and = & 똑같다
+print((3 > 0) and (3 > 5)) 
+print((3 > 0) & (3 < 5))
+
+print((3 > 0) or (3 > 5)) # 둘 중 하나만 만족하면 True  
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
